27d7249bb7fdfd199c3021084c037221cd79bba3
Matteo Perrone <teo.perro01@gmail.com>
2026-05-20 20:52:29 +0200

    4_complete

--- 04_conta_vocali/script.js
+++ 04_conta_vocali/script.js
@@ -1,13 +1,22 @@
 /* Scrivi una funzione che accetti una stringa e restituisca il numero di vocali contenute al suo interno */
 
 const word = 'javascript';
-
+const vocali = ['a', 'e', 'i', 'o', 'u'];
 
 // Dichiara la funzione qui.
 
+const contaVocali= (parola) => {
+    let total=0;
+    for (let i= 0; i<parola.length; i+=1){
+        if (vocali.includes(parola[i])){
+            total+=1
+        }
+    }
+    return total;
+}
 
 // Invoca la funzione qui e stampa il risultato in console
 
 
-
+console.log(contaVocali(word)); 
 //Risultato atteso se si passa 'javascript': 3 (a, a, i)
