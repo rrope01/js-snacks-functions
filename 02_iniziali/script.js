580581f33cdf467b96f1bb52bf501a3a224842a7
Matteo Perrone <teo.perro01@gmail.com>
2026-05-20 20:33:25 +0200

    2_complete

--- 02_iniziali/script.js
+++ 02_iniziali/script.js
@@ -5,9 +5,16 @@ const names = ["Anna", "Luca", "Marco", "Adele", "Giovanni", "Alessandra"];
 
 // Dichiara la funzione qui.
 
+const getInit= (ar) => {
+    for (let i = 0; i < ar.length; i++) {
+        ar[i] = ar[i][0];
+    }
+    return ar;
+}
 
 // Invoca la funzione qui e stampa il risultato in console
 
+console.log(getInit(names));
 
 
 //Risultato atteso: ["A", "L", "M", "A", "G", "A"]
