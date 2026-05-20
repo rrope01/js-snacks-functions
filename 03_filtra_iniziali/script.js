a1e271b70bbc33df3bee66745636f0b127e604a1
Matteo Perrone <teo.perro01@gmail.com>
2026-05-20 20:46:00 +0200

    3_complete

--- 03_filtra_iniziali/script.js
+++ 03_filtra_iniziali/script.js
@@ -5,9 +5,23 @@ const names = ["Anna", "Luca", "Marco", "Adele", "Laura", "Alessandra"];
 
 // Dichiara la funzione qui.
 
+const arrLet= (ar, letter) => {
+    for (let i = 0; i < ar.length; i++) {
+        if (ar[i][0] !== letter) {
+            ar.splice(i, 1);
+            i--;
+        }
+    }
+    return ar;
+}
 
 // Invoca la funzione qui e stampa il risultato in console
 
-
+let lettera = prompt("Inserisci una lettera:").toUpperCase();
+while (!isNaN(lettera) || lettera.length !== 1) {
+    alert("Per favore, inserisci una singola LETTERA.");
+    lettera = prompt("Inserisci una lettera:").toUpperCase();
+}
+console.log(arrLet(names, lettera));
 
 //Risultato atteso se si passa la lettera A: ["Anna", "Adele", "Alessandra"]
